11c3e052716721408ba70b814a88f0c863bba700
Diego Lopez <deigolopez@yahoo.com>
2015-09-05 01:10:38 -0400

    added email form and controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,18 @@
 class ContactsController < ApplicationController
-  
-
 
   def new
   	@contact = Contact.new
   end
   
   def create
-  	@contact = Contact.new(contact_params)
-
+  	@contact = Contact.new(contact_params)  	
   	if @contact.save
+  		name = params[:contact][:name]
+    	email = params[:contact][:email]
+    	body = params[:contact][:comments]
+
+    	ContactMailer.contact_email(name, email, body).deliver
+
   		flash[:success] = "Message Sent"
   		redirect_to new_contact_path
   	else
@@ -23,4 +26,5 @@ class ContactsController < ApplicationController
 	  def contact_params
 	  	params.require(:contact).permit(:name, :email, :comments)
 	  end
+
 end
